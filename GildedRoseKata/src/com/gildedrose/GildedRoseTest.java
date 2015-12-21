3666f09df51d4eff3decf5888d60cfb2b0d02b84
José Carlos Gil Zambrana <josecgil@gmail.com>
2015-12-21 18:09:10 +0100

    Added Gilded Rose full suite of tests

--- GildedRoseKata/src/com/gildedrose/GildedRoseTest.java
+++ GildedRoseKata/src/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,107 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void IfItemIsNormalSellInDecreasesByAtLeastOne() {
+        Item[] items = new Item[] { new Item("Normal", 1, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertTrue(app.items[0].sellIn < 1);
     }
+    
+    @Test
+	public void IfItemIsSulfurasSellInDoesNotDecrease() throws Exception {
+    	Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 1, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1, app.items[0].sellIn);
+	}
+    
+    @Test
+	public void IfSulfurasQualityDoesNotDecrease() throws Exception {
+    	Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 1, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality); 
+	}
+    
+    @Test
+	public void IfItemIsNormalQualityDecreasesByAtLeastOne() throws Exception {
+    	Item[] items = new Item[] { new Item("Normal", 0, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality < 1);
+	}
+    
+    @Test
+	public void IfItemIsBrieQualityDoesNotDecrease() throws Exception {
+    	Item[] items = new Item[] { new Item("Aged Brie", 0, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality >= 1);
+	}
+    
+    @Test
+    public void IfItemIsBrieAndExpiredQualityIncreasesByTwo() throws Exception {
+    	Item[] items = new Item[] { new Item("Aged Brie", 0, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(3, app.items[0].quality);
+	}
+    
+    @Test
+    public void IfItemIsBrieAndExpiredAndMaxedInQualityThenQualityDoesNotIncrease() throws Exception {
+    	Item[] items = new Item[] { new Item("Aged Brie", 0, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+	}
+    
+    @Test
+	public void IfItemIsConcertTicketWithMoreThan10DaysThenQualityIncreasesByOne() throws Exception {
+    	Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 11, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(2, app.items[0].quality);
+	}
+    
+    @Test
+	public void IfItemIsConcertTicketWithMoreThan5DaysThenQualityShouldIncreaseByTwo() throws Exception {
+    	Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 6, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(3, app.items[0].quality);
+	}
+    
+    @Test
+	public void IfItemIsConcertTicketWith5DaysOrLessThenQualityShouldIncreaseByThree() throws Exception {
+    	Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(4, app.items[0].quality);
+	}
+    
+    @Test
+	public void IfItemIsConcertTicketAndExpiredQualityShouldDropToZero() throws Exception {
+    	Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+	}
+    
+    @Test
+	public void IfItemIsNormalAndExpiredQualityShouldDecreaseByTwo() throws Exception {
+    	Item[] items = new Item[] { new Item("Normal", 0, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(3, app.items[0].quality);
+	}
+    
+    @Test
+	public void IfItemIsSulfurasExpiredQualityDoesNotDecrease() throws Exception {
+    	Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", -1, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+	}
 
 }
